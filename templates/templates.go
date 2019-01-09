f9dba22200161b3b94144095057d64be398c1d2c
Murray Resinski <murray.resinski@octanner.com>
2019-01-09 14:57:31 -0700

    updated to use unipool for routers

--- templates/templates.go
+++ templates/templates.go
@@ -59,13 +59,18 @@ set LogString "$LogString hostname={{$value.NewHost}} site_path=[HTTP::path]"
 HTTP::header insert X-Orig-Path "{{$value.Path}}"
 HTTP::path [string map -nocase {"{{$value.Path}}/" "{{$value.ReplacePath}}/"} [HTTP::path]]
 if {[regsub -all "//" [HTTP::path] "/" newpath] > 0} { HTTP::path $newpath }
-pool {{$value.Pool}}
+#oldpool:pool {{$value.Pool}}
+set new_port "{{$value.Nodeport}}"
+pool {{$value.Unipool}}
+
 }
 "{{$value.Path}}*" {
 set LogString "$LogString hostname={{$value.NewHost}} site_path=[HTTP::uri]"
 HTTP::header insert X-Orig-Path "{{$value.Path}}"
 HTTP::uri [string map -nocase {"{{$value.Path}}" "{{$value.ReplacePath}}"} [HTTP::uri]]
-pool {{$value.Pool}}
+#oldpool:pool {{$value.Pool}}
+set new_port "{{$value.Nodeport}}"
+pool {{$value.Unipool}}
 }
 {{end}}
 			}
