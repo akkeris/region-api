bf2cd9137cca93c13e4bc9830863de10c2b2c98b
Murray Resinski <murray.resinski@octanner.com>
2020-03-24 16:10:40 -0600

    decom cassandra

--- service/configvars.go
+++ service/configvars.go
@@ -52,14 +52,6 @@ func GetServiceConfigVars(db *sql.DB, appname string, space string, appbindings 
 			for key, value := range vars {
 				servicevars = append(servicevars, structs.EnvVar{Name: key, Value: value.(string)})
 			}
-		} else if servicetype == "cassandra" {
-			vars, err := GetCassandraVars(servicename)
-			if err != nil {
-				return err, elist
-			}
-			for key, value := range vars {
-				servicevars = append(servicevars, structs.EnvVar{Name: key, Value: value.(string)})
-			}
 			// if nothing else matches see if we match an
 			// open service broker that dynamically registered.
 		} else if IsOSBService(servicetype) {
