beac3f2be037748138019d51430f1e2e74d627e4
Sam Beckett <sbeck14@me.com>
2020-07-27 11:47:27 -0600

    move service creation to its own function

--- deployment/deployment.go
+++ deployment/deployment.go
@@ -564,6 +564,225 @@ func AddDeploymentV2(db *sql.DB, params martini.Params, deployment structs.AppDe
 	r.JSON(http.StatusCreated, newDeployment)
 }
 
+// createOrUpdateService - Create or update service for a given deployment and runtime
+func createOrUpdateService(db *sql.DB, rt runtime.Runtime, payload structs.DeploySpecV2) error {
+	serviceExists, err := rt.ServiceExists(payload.Space, payload.Name)
+	if err != nil {
+		return err
+	}
+
+	isInternal, err := utils.IsInternalSpace(db, payload.Space)
+	if err != nil {
+		return err
+	}
+
+	appIngress, err := ingress.GetAppIngress(db, isInternal)
+	if err != nil {
+		return err
+	}
+
+	siteIngress, err := ingress.GetSiteIngress(db, isInternal)
+	if err != nil {
+		return err
+	}
+
+	// Any deployment features requiring istio transitioned ingresses should
+	// be marked here. Only apply this to the web dyno types.
+
+	appFQDN := payload.Name + "-" + payload.Space
+	if payload.Space == "default" {
+		appFQDN = payload.Name
+	}
+	if isInternal {
+		appFQDN = appFQDN + "." + os.Getenv("INTERNAL_DOMAIN")
+	} else {
+		appFQDN = appFQDN + "." + os.Getenv("EXTERNAL_DOMAIN")
+	}
+
+	// Create/update service
+	if !serviceExists {
+		if err := rt.CreateService(payload.Space, payload.Name, payload.Port, payload.Labels, payload.Features); err != nil {
+			return err
+		}
+	} else {
+		if err := rt.UpdateService(payload.Space, payload.Name, payload.Port, payload.Labels, payload.Features); err != nil {
+			return err
+		}
+	}
+
+	// Apply the HTTP filters
+	foundJwtFilter := false
+	foundCorsFilter := false
+	foundCspFilter := false
+	for _, filter := range payload.Filters {
+		if filter.Type == "jwt" {
+			issuer := ""
+			jwksURI := ""
+			audiences := make([]string, 0)
+			excludes := make([]string, 0)
+			includes := make([]string, 0)
+			if val, ok := filter.Data["issuer"]; ok {
+				issuer = val
+			}
+			if val, ok := filter.Data["jwks_uri"]; ok {
+				jwksURI = val
+			}
+			if val, ok := filter.Data["audiences"]; ok {
+				if val != "" {
+					audiences = strings.Split(val, ",")
+				}
+			}
+			if val, ok := filter.Data["excludes"]; ok {
+				if val != "" {
+					excludes = strings.Split(val, ",")
+				}
+			}
+			if val, ok := filter.Data["includes"]; ok {
+				if val != "" {
+					includes = strings.Split(val, ",")
+				}
+			}
+			if jwksURI == "" {
+				fmt.Printf("WARNING: Invalid jwt configuration, uri was not valid: %s\n", jwksURI)
+			} else {
+				foundJwtFilter = true
+				if err := appIngress.InstallOrUpdateJWTAuthFilter(payload.Name, payload.Space, appFQDN, int64(payload.Port), issuer, jwksURI, audiences, excludes, includes); err != nil {
+					fmt.Printf("WARNING: There was an error installing or updating JWT Auth filter: %s\n", err.Error())
+				}
+			}
+		} else if filter.Type == "cors" {
+			if os.Getenv("INGRESS_DEBUG") == "true" {
+				fmt.Printf("[ingress] Adding CORS filter %#+v\n", filter)
+			}
+			allowOrigin := make([]string, 0)
+			allowMethods := make([]string, 0)
+			allowHeaders := make([]string, 0)
+			exposeHeaders := make([]string, 0)
+			maxAge := time.Second * 86400
+			allowCredentials := false
+			if val, ok := filter.Data["allow_origin"]; ok {
+				if val != "" {
+					allowOrigin = strings.Split(val, ",")
+				}
+			}
+			if val, ok := filter.Data["allow_methods"]; ok {
+				if val != "" {
+					allowMethods = strings.Split(val, ",")
+				}
+			}
+			if val, ok := filter.Data["allow_headers"]; ok {
+				if val != "" {
+					allowHeaders = strings.Split(val, ",")
+				}
+			}
+			if val, ok := filter.Data["expose_headers"]; ok {
+				if val != "" {
+					exposeHeaders = strings.Split(val, ",")
+				}
+			}
+			if val, ok := filter.Data["max_age"]; ok {
+				age, err := strconv.ParseInt(val, 10, 32)
+				if err == nil {
+					maxAge = time.Second * time.Duration(age)
+				} else {
+					fmt.Printf("WARNING: Unable to convert max_age to value %s\n", val)
+				}
+			}
+			if val, ok := filter.Data["allow_credentials"]; ok {
+				if val == "true" {
+					allowCredentials = true
+				} else {
+					allowCredentials = false
+				}
+			}
+			if err := appIngress.InstallOrUpdateCORSAuthFilter(payload.Name+"-"+payload.Space, "/", allowOrigin, allowMethods, allowHeaders, exposeHeaders, maxAge, allowCredentials); err != nil {
+				fmt.Printf("WARNING: There was an error installing or updating CORS Auth filter: %s\n", err.Error())
+			} else {
+				foundCorsFilter = true
+			}
+			routes, err := ingress.GetPathsByApp(db, payload.Name, payload.Space)
+			if err == nil {
+				for _, route := range routes {
+					if err := siteIngress.InstallOrUpdateCORSAuthFilter(route.Domain, route.Path, allowOrigin, allowMethods, allowHeaders, exposeHeaders, maxAge, allowCredentials); err != nil {
+						fmt.Printf("WARNING: There was an error installing or updating CORS Auth filter on site: %s: %s\n", route.Domain, err.Error())
+					}
+				}
+			} else {
+				fmt.Printf("WARNING: There was an error trying to pull the routes for an app to install the CORS auth filters on: %s\n", err.Error())
+			}
+		} else if filter.Type == "csp" {
+			if os.Getenv("INGRESS_DEBUG") == "true" {
+				fmt.Printf("[ingress] Adding CSP filter %#+v\n", filter)
+			}
+			policy := ""
+			if val, ok := filter.Data["policy"]; ok {
+				policy = val
+			}
+			if policy != "" {
+				if err := appIngress.InstallOrUpdateCSPFilter(payload.Name+"-"+payload.Space, "/", policy); err != nil {
+					fmt.Printf("WARNING: There was an error installing or updating CORS Auth filter: %s\n", err.Error())
+				} else {
+					foundCspFilter = true
+				}
+				routes, err := ingress.GetPathsByApp(db, payload.Name, payload.Space)
+				if err == nil {
+					for _, route := range routes {
+						if err := siteIngress.InstallOrUpdateCSPFilter(route.Domain, route.Path, policy); err != nil {
+							fmt.Printf("WARNING: There was an error installing or updating CSP filter on site: %s: %s\n", route.Domain, err.Error())
+						}
+					}
+				} else {
+					fmt.Printf("WARNING: There was an error trying to pull the routes for an app to install the CSP filters on: %s\n", err.Error())
+				}
+			}
+		} else {
+			fmt.Printf("WARNING: Unknown filter type: %s\n", filter.Type)
+		}
+	}
+
+	// If we don't have a CORS filter remove it from the app and any sites it may be associated with.
+	// this is effectively a no-op if there is no CORS auth filter in the first place
+	if !foundCorsFilter {
+		if err := appIngress.DeleteCORSAuthFilter(payload.Name+"-"+payload.Space, "/"); err != nil {
+			fmt.Printf("WARNING: There was an error removing the CORS auth filter from the app: %s\n", err.Error())
+		}
+		routes, err := ingress.GetPathsByApp(db, payload.Name, payload.Space)
+		if err == nil {
+			for _, route := range routes {
+				if err := siteIngress.DeleteCORSAuthFilter(route.Domain, route.Path); err != nil {
+					fmt.Printf("WARNING: There was an error removing CORS Auth filter on site: %s: %s\n", route.Domain, err.Error())
+				}
+			}
+		} else {
+			fmt.Printf("WARNING: There was an error trying to pull the routes for an app to install the CORS auth filters on: %s\n", err.Error())
+		}
+	}
+
+	if !foundCspFilter {
+		if err := appIngress.DeleteCSPFilter(payload.Name+"-"+payload.Space, "/"); err != nil {
+			fmt.Printf("WARNING: There was an error removing the CSP filter from the app: %s\n", err.Error())
+		}
+		routes, err := ingress.GetPathsByApp(db, payload.Name, payload.Space)
+		if err == nil {
+			for _, route := range routes {
+				if err := siteIngress.DeleteCSPFilter(route.Domain, route.Path); err != nil {
+					fmt.Printf("WARNING: There was an error removing CSP filter on site: %s: %s\n", route.Domain, err.Error())
+				}
+			}
+		} else {
+			fmt.Printf("WARNING: There was an error trying to pull the routes for an app to install the CSP filters on: %s\n", err.Error())
+		}
+	}
+
+	if !foundJwtFilter {
+		if err := appIngress.DeleteJWTAuthFilter(payload.Name, payload.Space, appFQDN, int64(payload.Port)); err != nil {
+			fmt.Printf("WARNING: There was an error removing the JWT auth filter: %s\n", err.Error())
+		}
+	}
+
+	return nil
+}
+
 // DeploymentV2Handler - HTTP handler for DeploymentV2
 func DeploymentV2Handler(db *sql.DB, payload structs.DeploySpecV2, berr binding.Errors, r render.Render) {
 	if berr != nil {
@@ -665,7 +884,7 @@ func DeploymentV2(db *sql.DB, payload structs.DeploySpecV2) (structs.Deployrespo
 		elist = append(elist, e)
 	}
 
-	// We have everything we need to create a one-off pod.
+	// We have everything we need to create a one-off pod at this point
 	if isOneOff {
 		if rt.OneOffExists(payload.Space, payload.Name) {
 			rt.DeletePod(payload.Space, payload.Name)
@@ -740,15 +959,6 @@ func DeploymentV2(db *sql.DB, payload structs.DeploySpecV2) (structs.Deployrespo
 		}
 	}
 
-	appIngress, err := ingress.GetAppIngress(db, isInternal)
-	if err != nil {
-		return deployresponse, http.StatusInternalServerError, err
-	}
-	siteIngress, err := ingress.GetSiteIngress(db, isInternal)
-	if err != nil {
-		return deployresponse, http.StatusInternalServerError, err
-	}
-
 	// Create deployment
 	var deployment structs.Deployment
 	deployment.Labels = payload.Labels
@@ -783,10 +993,6 @@ func DeploymentV2(db *sql.DB, payload structs.DeploySpecV2) (structs.Deployrespo
 	if err != nil {
 		return deployresponse, http.StatusInternalServerError, err
 	}
-	serviceExists, err := rt.ServiceExists(space, name)
-	if err != nil {
-		return deployresponse, http.StatusInternalServerError, err
-	}
 
 	// Do not write to cluster above this line, everything below should apply changes,
 	// everything above should do sanity checks, this helps prevent "half" deployments
@@ -802,217 +1008,26 @@ func DeploymentV2(db *sql.DB, payload structs.DeploySpecV2) (structs.Deployrespo
 		}
 	}
 
-	// Any deployment features requiring istio transitioned ingresses should
-	// be marked here. Only apply this to the web dyno types.
-	appFQDN := name + "-" + space
-	if space == "default" {
-		appFQDN = name
-	}
-	if isInternal {
-		appFQDN = appFQDN + "." + os.Getenv("INTERNAL_DOMAIN")
-	} else {
-		appFQDN = appFQDN + "." + os.Getenv("EXTERNAL_DOMAIN")
-	}
-
-	// Create/update service
+	// Create/update service for web dyno types
 	if finalport != -1 {
-		if !serviceExists {
-			if err := rt.CreateService(space, name, finalport, payload.Labels, payload.Features); err != nil {
-				return deployresponse, http.StatusInternalServerError, err
-			}
-		} else {
-			if err := rt.UpdateService(space, name, finalport, payload.Labels, payload.Features); err != nil {
-				return deployresponse, http.StatusInternalServerError, err
-			}
-		}
-
-		// Apply the HTTP filters
-		foundJwtFilter := false
-		foundCorsFilter := false
-		foundCspFilter := false
-		for _, filter := range payload.Filters {
-			if filter.Type == "jwt" {
-				issuer := ""
-				jwksURI := ""
-				audiences := make([]string, 0)
-				excludes := make([]string, 0)
-				includes := make([]string, 0)
-				if val, ok := filter.Data["issuer"]; ok {
-					issuer = val
-				}
-				if val, ok := filter.Data["jwks_uri"]; ok {
-					jwksURI = val
-				}
-				if val, ok := filter.Data["audiences"]; ok {
-					if val != "" {
-						audiences = strings.Split(val, ",")
-					}
-				}
-				if val, ok := filter.Data["excludes"]; ok {
-					if val != "" {
-						excludes = strings.Split(val, ",")
-					}
-				}
-				if val, ok := filter.Data["includes"]; ok {
-					if val != "" {
-						includes = strings.Split(val, ",")
-					}
-				}
-				if jwksURI == "" {
-					fmt.Printf("WARNING: Invalid jwt configuration, uri was not valid: %s\n", jwksURI)
-				} else {
-					foundJwtFilter = true
-					if err := appIngress.InstallOrUpdateJWTAuthFilter(name, space, appFQDN, int64(finalport), issuer, jwksURI, audiences, excludes, includes); err != nil {
-						fmt.Printf("WARNING: There was an error installing or updating JWT Auth filter: %s\n", err.Error())
-					}
-				}
-			} else if filter.Type == "cors" {
-				if os.Getenv("INGRESS_DEBUG") == "true" {
-					fmt.Printf("[ingress] Adding CORS filter %#+v\n", filter)
-				}
-				allowOrigin := make([]string, 0)
-				allowMethods := make([]string, 0)
-				allowHeaders := make([]string, 0)
-				exposeHeaders := make([]string, 0)
-				maxAge := time.Second * 86400
-				allowCredentials := false
-				if val, ok := filter.Data["allow_origin"]; ok {
-					if val != "" {
-						allowOrigin = strings.Split(val, ",")
-					}
-				}
-				if val, ok := filter.Data["allow_methods"]; ok {
-					if val != "" {
-						allowMethods = strings.Split(val, ",")
-					}
-				}
-				if val, ok := filter.Data["allow_headers"]; ok {
-					if val != "" {
-						allowHeaders = strings.Split(val, ",")
-					}
-				}
-				if val, ok := filter.Data["expose_headers"]; ok {
-					if val != "" {
-						exposeHeaders = strings.Split(val, ",")
-					}
-				}
-				if val, ok := filter.Data["max_age"]; ok {
-					age, err := strconv.ParseInt(val, 10, 32)
-					if err == nil {
-						maxAge = time.Second * time.Duration(age)
-					} else {
-						fmt.Printf("WARNING: Unable to convert max_age to value %s\n", val)
-					}
-				}
-				if val, ok := filter.Data["allow_credentials"]; ok {
-					if val == "true" {
-						allowCredentials = true
-					} else {
-						allowCredentials = false
-					}
-				}
-				if err := appIngress.InstallOrUpdateCORSAuthFilter(name+"-"+space, "/", allowOrigin, allowMethods, allowHeaders, exposeHeaders, maxAge, allowCredentials); err != nil {
-					fmt.Printf("WARNING: There was an error installing or updating CORS Auth filter: %s\n", err.Error())
-				} else {
-					foundCorsFilter = true
-				}
-				routes, err := ingress.GetPathsByApp(db, name, space)
-				if err == nil {
-					for _, route := range routes {
-						if err := siteIngress.InstallOrUpdateCORSAuthFilter(route.Domain, route.Path, allowOrigin, allowMethods, allowHeaders, exposeHeaders, maxAge, allowCredentials); err != nil {
-							fmt.Printf("WARNING: There was an error installing or updating CORS Auth filter on site: %s: %s\n", route.Domain, err.Error())
-						}
-					}
-				} else {
-					fmt.Printf("WARNING: There was an error trying to pull the routes for an app to install the CORS auth filters on: %s\n", err.Error())
-				}
-			} else if filter.Type == "csp" {
-				if os.Getenv("INGRESS_DEBUG") == "true" {
-					fmt.Printf("[ingress] Adding CSP filter %#+v\n", filter)
-				}
-				policy := ""
-				if val, ok := filter.Data["policy"]; ok {
-					policy = val
-				}
-				if policy != "" {
-					if err := appIngress.InstallOrUpdateCSPFilter(name+"-"+space, "/", policy); err != nil {
-						fmt.Printf("WARNING: There was an error installing or updating CORS Auth filter: %s\n", err.Error())
-					} else {
-						foundCspFilter = true
-					}
-					routes, err := ingress.GetPathsByApp(db, name, space)
-					if err == nil {
-						for _, route := range routes {
-							if err := siteIngress.InstallOrUpdateCSPFilter(route.Domain, route.Path, policy); err != nil {
-								fmt.Printf("WARNING: There was an error installing or updating CSP filter on site: %s: %s\n", route.Domain, err.Error())
-							}
-						}
-					} else {
-						fmt.Printf("WARNING: There was an error trying to pull the routes for an app to install the CSP filters on: %s\n", err.Error())
-					}
-				}
-			} else {
-				fmt.Printf("WARNING: Unknown filter type: %s\n", filter.Type)
-			}
-		}
-
-		// If we don't have a CORS filter remove it from the app and any sites it may be associated with.
-		// this is effectively a no-op if there is no CORS auth filter in the first place
-		if !foundCorsFilter {
-			if err := appIngress.DeleteCORSAuthFilter(name+"-"+space, "/"); err != nil {
-				fmt.Printf("WARNING: There was an error removing the CORS auth filter from the app: %s\n", err.Error())
-			}
-			routes, err := ingress.GetPathsByApp(db, name, space)
-			if err == nil {
-				for _, route := range routes {
-					if err := siteIngress.DeleteCORSAuthFilter(route.Domain, route.Path); err != nil {
-						fmt.Printf("WARNING: There was an error removing CORS Auth filter on site: %s: %s\n", route.Domain, err.Error())
-					}
-				}
-			} else {
-				fmt.Printf("WARNING: There was an error trying to pull the routes for an app to install the CORS auth filters on: %s\n", err.Error())
-			}
-		}
-		if !foundCspFilter {
-			if err := appIngress.DeleteCSPFilter(name+"-"+space, "/"); err != nil {
-				fmt.Printf("WARNING: There was an error removing the CSP filter from the app: %s\n", err.Error())
-			}
-			routes, err := ingress.GetPathsByApp(db, name, space)
-			if err == nil {
-				for _, route := range routes {
-					if err := siteIngress.DeleteCSPFilter(route.Domain, route.Path); err != nil {
-						fmt.Printf("WARNING: There was an error removing CSP filter on site: %s: %s\n", route.Domain, err.Error())
-					}
-				}
-			} else {
-				fmt.Printf("WARNING: There was an error trying to pull the routes for an app to install the CSP filters on: %s\n", err.Error())
-			}
-		}
-		if !foundJwtFilter {
-			if err := appIngress.DeleteJWTAuthFilter(name, space, appFQDN, int64(finalport)); err != nil {
-				fmt.Printf("WARNING: There was an error removing the JWT auth filter: %s\n", err.Error())
-			}
+		if err = createOrUpdateService(db, rt, payload); err != nil {
+			return deployresponse, http.StatusInternalServerError, err
 		}
 	}
 
-	// Prepare the response back
+	// Prepare response back
+	if finalport != -1 {
+		deployresponse.Service = "Service Created"
+	} else {
+		deployresponse.Service = "Service not required"
+	}
+
 	if !deploymentExists {
-		if finalport != -1 {
-			deployresponse.Service = "Service Created"
-		}
-		if port == -1 {
-			deployresponse.Service = "Service not required"
-		}
 		deployresponse.Controller = "Deployment Created"
 	} else {
-		if finalport != -1 {
-			deployresponse.Service = "Service Created"
-		}
-		if finalport == -1 {
-			deployresponse.Service = "Service not required"
-		}
 		deployresponse.Controller = "Deployment Updated"
 	}
+
 	return deployresponse, http.StatusCreated, nil
 }
 
